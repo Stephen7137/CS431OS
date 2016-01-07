882091bf713bc69701384cd290e3f00ee8081cc4
Stephen Jackson <EagleEE7137@gmail.com>
2016-01-06 19:24:00 -0800

    Setup OS
    Create the project and add command to get input string and parse the
    line.

---
+++ 431OS/431OS/Kernel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace _431OS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+        }
+
+        protected override void Run()
+        {
+            var input = Console.ReadLine();
+            command(input);
+        }
+
+        private void command(String input)
+        {
+            String[] command = input.Split(' ');
+            switch (command[0])
+            {
+                case "echo":
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Not a valid input.");
+                    break;
+            }
+        }
+    }
+}
